c511f8ecfcb7c0289561acfbc92465bb481fe347
vinaykannambadi <vinaykannambadi@gmail.com>
2019-12-18 13:30:35 +0530

    Prod-cart-stage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { ProductListComponent } from "./product-list/product-list.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
 import { RouterModule } from "@angular/router";
-import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { CartComponent } from "./cart/cart.component";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductListComponent,
     TopBarComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +26,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
-      { path: "products/:productId", component: ProductDetailsComponent }
+      { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "cart", component: CartComponent }
     ])
   ],
   providers: [],
